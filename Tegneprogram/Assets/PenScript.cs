9d76d2cc1306c8d40d2c53fc12e6e6f9c7e2b096
Sputnikboi <Guppymund@gmail.com>
2023-02-13 10:01:27 +0100

    smoother lines and less objects

--- Tegneprogram/Assets/PenScript.cs
+++ Tegneprogram/Assets/PenScript.cs
@@ -10,6 +10,7 @@ public class PenScript : MonoBehaviour
     private Camera cam;
     public GameObject obj;
     public GameObject circ;
+    int curPos = 0;
     GameObject lineRendererObj;
     GameObject circObj;
     LineRenderer lineRenderer;
@@ -17,6 +18,7 @@ public class PenScript : MonoBehaviour
     void Start()
     {
         cam = Camera.main;
+        curPos=1;
     }
 
     // Update is called once per frame
@@ -26,11 +28,11 @@ public class PenScript : MonoBehaviour
         mousePos=Input.mousePosition;
         this.transform.position = cam.ScreenToWorldPoint(new Vector3(mousePos[0], mousePos[1], 0));
         curPoint=cam.ScreenToWorldPoint(new Vector3(mousePos[0], mousePos[1], 0));
-        if(Input.GetMouseButton(0)){
-            //Instantiate(obj,this.transform.position,this.transform.rotation);
-            //lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
-            circObj = Instantiate(circ,curPoint,this.transform.rotation);
-            /*lineRendererObj= Instantiate(obj,this.transform.position,this.transform.rotation);
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            Instantiate(obj,this.transform.position,this.transform.rotation);
+            lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
+            //circObj = Instantiate(circ,curPoint,this.transform.rotation);
+            lineRendererObj= Instantiate(obj,this.transform.position,this.transform.rotation);
 
             lineRenderer=lineRendererObj.GetComponent<LineRenderer>();
             lineRenderer.startColor = Color.black;
@@ -40,7 +42,14 @@ public class PenScript : MonoBehaviour
             lineRenderer.positionCount = 2;
             lineRenderer.useWorldSpace = true;
             lineRenderer.SetPosition(0, new Vector3(prevMousePos[0],prevMousePos[1],0)); //x,y and z position of the starting point of the line
-            lineRenderer.SetPosition(1, new Vector3(curPoint[0],curPoint[1],0));*/
+            lineRenderer.SetPosition(1, new Vector3(curPoint[0],curPoint[1],0));
+        } else if(Input.GetKey(KeyCode.Mouse0)){
+            lineRenderer.positionCount++;
+            curPos++;
+            lineRenderer.SetPosition(curPos, new Vector3(curPoint[0],curPoint[1],0));
+        } else{
+            lineRenderer= null;
+            curPos =1;
         }
     }
 }
