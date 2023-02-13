dcfd7de9f1675a533a332f853d8a4dc8cb684ae2
Sputnikboi <Guppymund@gmail.com>
2023-02-13 09:05:20 +0100

    made work

--- Tegneprogram/Assets/PenScript.cs
+++ Tegneprogram/Assets/PenScript.cs
@@ -9,7 +9,9 @@ public class PenScript : MonoBehaviour
     Vector3 prevMousePos;
     private Camera cam;
     public GameObject obj;
+    public GameObject circ;
     GameObject lineRendererObj;
+    GameObject circObj;
     LineRenderer lineRenderer;
     // Start is called before the first frame update
     void Start()
@@ -18,7 +20,7 @@ public class PenScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         prevMousePos =  cam.ScreenToWorldPoint(new Vector3(mousePos[0], mousePos[1], 0));
         mousePos=Input.mousePosition;
@@ -28,11 +30,12 @@ public class PenScript : MonoBehaviour
             //Instantiate(obj,this.transform.position,this.transform.rotation);
             //lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
             lineRendererObj= Instantiate(obj,this.transform.position,this.transform.rotation);
+            circObj = Instantiate(circ,this.transform.position,this.transform.rotation);
             lineRenderer=lineRendererObj.GetComponent<LineRenderer>();
             lineRenderer.startColor = Color.black;
             lineRenderer.endColor = Color.black;
-            lineRenderer.startWidth = 1f;
-            lineRenderer.endWidth = 1f;
+            lineRenderer.startWidth = 0.20f;
+            lineRenderer.endWidth = 0.20f;
             lineRenderer.positionCount = 2;
             lineRenderer.useWorldSpace = true;
             lineRenderer.SetPosition(0, new Vector3(prevMousePos[0],prevMousePos[1],0)); //x,y and z position of the starting point of the line
