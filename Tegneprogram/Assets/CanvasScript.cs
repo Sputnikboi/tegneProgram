529f67e702b8c108e7957c82234b8084cd7ea695
Sputnikboi <Guppymund@gmail.com>
2023-03-06 12:46:04 +0100

    Fixed start menu

--- Tegneprogram/Assets/CanvasScript.cs
+++ Tegneprogram/Assets/CanvasScript.cs
@@ -22,11 +22,12 @@ public class CanvasScript : MonoBehaviour
         UiElements[3] = GameObject.Find("WidthText");
         UiElements[4] = GameObject.Find("SubmitButton");
         UiElements[5] = GameObject.Find("StartPanel");
+        UiElements[6] = GameObject.Find("StartBackground");
         height =UiElements[0].GetComponent<HeightSize>();
         width = UiElements[1].GetComponent<WidthSize>();
         transform.localScale = new Vector3(width.width/100,height.height/100,1);
 
-        for(int i=0;i <6; i++){
+        for(int i=0;i <7; i++){
             UiElements[i].SetActive(false);
    
         }
